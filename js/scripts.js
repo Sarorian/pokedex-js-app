01641a90e9718dc533c915b2ce8ba1e9d47d4af0
Sarorian <shamowen5@gmail.com>
2023-03-08 02:26:05 -0700

    Removed tutorial stuff, added framework for pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-alert('Hello world');
-let favoiteFood = "Banana";
-document.write(favoiteFood);
+let pokemonList = [];
+pokemonList[0] = {
+    name: "Bulbasaur", 
+    height: 0.7, 
+    types: ["grass", "poison"]
+};
+pokemonList[1] = {
+    name: "Ivysaur", 
+    height: 1, 
+    types: ["grass", "poison"]
+};
+pokemonList[2] = {
+    name: "Venusaur", 
+    height: 2, 
+    types: ["grass", "poison"]
+};
