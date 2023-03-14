c5c8541766f179faa9374f5b957c7dd28415e555
Sarorian <shamowen5@gmail.com>
2023-03-14 00:51:27 -0600

    Added IIFE, updated forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,61 @@
-let pokemonList = [];
-pokemonList[0] = {
-    name: "Bulbasaur", 
-    height: 0.7, 
-    types: ["grass", "poison"]
-};
-pokemonList[1] = {
-    name: "Ivysaur", 
-    height: 1, 
-    types: ["grass", "poison"]
-};
-pokemonList[2] = {
-    name: "Venusaur", 
-    height: 2, 
-    types: ["grass", "poison"]
-};
-// for (let i = 0; i < pokemonList.length; i++) {
-//     /*assigning varibles for current Pokemon's name and height*/
-//     let name = pokemonList[i].name;
-//     let height = pokemonList[i].height;
+// let pokemonList = [];
+// pokemonList[0] = {
+//     name: "Bulbasaur", 
+//     height: 0.7, 
+//     types: ["grass", "poison"]
+// };
+// pokemonList[1] = {
+//     name: "Ivysaur", 
+//     height: 1, 
+//     types: ["grass", "poison"]
+// };
+// pokemonList[2] = {
+//     name: "Venusaur", 
+//     height: 2, 
+//     types: ["grass", "poison"]
+// };
 
-//     /*Writing name and height to document*/
-//     document.write(name + " " + height);
+let pokemonRepository = (function() {
+    let pokemonList = [];
+    pokemonList[0] = {
+        name: "Bulbasaur", 
+        height: 0.7, 
+        types: ["grass", "poison"]
+    };
+    pokemonList[1] = {
+        name: "Ivysaur", 
+        height: 1, 
+        types: ["grass", "poison"]
+    };
+    pokemonList[2] = {
+        name: "Venusaur", 
+        height: 2, 
+        types: ["grass", "poison"]
+    };
 
-//     /*Testing if height is over 1m*/
-//     if (height > 1) {
-//         document.write(" - Wow, that's big!");
-//     }
-//     document.write("<br>");
-// }
+    function getAll() {
+        return pokemonList;
+    }
+    
+    function add(pokemon) {
+        if (typeof(pokemon) === "object"){
+            if (Object.keys(pokemon) === ["name", "height", "types"]) {
+                pokemonList.push(pokemon);
+            } else {
+                return "Please give the Pokemon a name, height, and types";
+            }
+        } else {
+            return "Please make the Pokemon an object";
+        }
+    }  
 
-pokemonList.forEach(function(pokemon) {
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + " " + pokemon.height);
     if (pokemon.height > 1) {
          document.write(" - Wow, that's big!");
